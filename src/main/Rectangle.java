cba18bfd9a151fcbaff9129212917f7fe6a54890
m.chraghalikhani <miladalikhani76@gmail.com>
2021-06-26 04:45:58 +0430

    add setter and getter

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -10,6 +10,22 @@ public class Rectangle {
         this.width = width;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
     public int computeArea()
     {
         return this.height * this.width;
--- src/tests/main/RectangleTest.java
+++ src/tests/main/RectangleTest.java
@@ -9,6 +9,19 @@ class RectangleTest {
     void computeAreaTest(){
         Rectangle rectangle = new Rectangle(10, 20);
         Assert.assertEquals(200, rectangle.computeArea());
+        rectangle.setHeight(20);
+        rectangle.setWidth(30);
+        Assert.assertEquals(600, rectangle.computeArea());
+    }
+
+    @Test
+    void changeWidthandHeightTest()
+    {
+        Rectangle rectangle = new Rectangle(10, 20);
+        rectangle.setHeight(20);
+        rectangle.setWidth(30);
+        Assert.assertEquals(20, rectangle.getHeight());
+        Assert.assertEquals(30, rectangle.getWidth());
     }
 
 }
